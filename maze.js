b2467b155e86d416db0cf34309739baca0b68e5e
Dale-Biggs <biggs_dale@yahoo.com>
2017-10-16 14:11:01 -0500

    Complete exercise 2: All walls turn red on contact

--- maze.js
+++ maze.js
@@ -4,19 +4,32 @@ window.onload= function(){
   var myBoundary1=document.getElementById('boundary1');
   var myBoundary=document.getElementsByClassName('boundary');
 
+
   elemStart.onmouseover= function(){
-    if (myBoundary1.classList[1]=="youlose"){
-      myBoundary1.classList.remove("youlose");
-    }else{
-      border1Contact();
+    for (var elem=0; elem<myBoundary.length-1; elem++){
+      if(myBoundary[elem].classList[1]=="youlose"){
+        myBoundary[elem].classList.remove("youlose");
+      }else{
+        borderContact();
+      }
     }
   }
 
-  function border1Contact(){
+  function borderContact(){
+    for(var i=0; i<myBoundary.length-1;i++){
+      myBoundary[i].addEventListener("mouseover", function(){
+        for(var elem=0; elem<myBoundary.length-1;elem++){
+          myBoundary[elem].classList.add("youlose");
+        }
+      });
+    };
+  }
+
+  /*function border1Contact(){
     myBoundary1.addEventListener("mouseover", function(){
       myBoundary1.classList.add("youlose");
       alert('You Lose');
     })
-  }
+  }*/
 
-}
+  }
